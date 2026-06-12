a2bfaf4540e845ec96ce3339e5857827c000f093
Artificial Dumbass <themarkup1592@gmail.com>
2026-06-12 14:58:23 +0700

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -51,7 +51,7 @@ export async function proxy(request: NextRequest) {
   const ip =
     request.headers.get("x-forwarded-for")?.split(",")[0].trim() ?? "anonymous";
 
-  let supabaseResponse = NextResponse.next({ request });
+  const supabaseResponse = NextResponse.next({ request });
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
